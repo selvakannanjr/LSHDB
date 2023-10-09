88d91bc350c9f871fee92afd048c495fb7ecf51e
selvakannanjr <4mselva@gmail.com>
2023-10-09 14:54:30 +0530

    updates

--- datastore.go
+++ datastore.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
 	"strconv"
 )
 
@@ -19,12 +20,30 @@ type ImageRec struct {
 	Vector  []float64
 }
 
+type ResType struct {
+	ImageID string  `json:"imageid"`
+	Score   float64 `json:"score"`
+}
+
 // a struct method to compute and return the cosine similarity with a given vector
 func (ir *ImageRec) CosineSimilarity(vector []float64) float64 {
 	return cosineSimilarity(ir.Vector, vector)
 }
 
 type LSHMap map[string][]ImageRec
+
+// a struct method to insert a new image record into the map
+func (L *LSHMap) InsertImageRec(ir ImageRec, matrix [][]float64) {
+	bid := ComputeBucketID(ir.Vector,matrix)
+	// if bid is not in L, add it to L
+	// if bid is in L, append the vector to the bucket
+	_, ok := (*L)[bid]
+	if !ok {
+		(*L)[bid] = []ImageRec{ir}
+	} else {
+		(*L)[bid] = append((*L)[bid], ir)
+	}
+}
 
 // a struct method to load the map from a csv file
 func (L *LSHMap) LoadMap(filename string, matrix [][]float64) {
@@ -42,19 +61,55 @@ func (L *LSHMap) LoadMap(filename string, matrix [][]float64) {
 			element, _ := strconv.ParseFloat(line[i], 64)
 			vector = append(vector, element)
 		}
-		bid := ComputeBucketID(vector,matrix)
-		// if bid is not in L, add it to L
-		// if bid is in L, append the vector to the bucket
-		_, ok := (*L)[bid]
-		if !ok {
-			(*L)[bid] = []ImageRec{{ImageID: line[0], Vector: vector}}
-		} else {
-			(*L)[bid] = append((*L)[bid], ImageRec{ImageID: line[0], Vector: vector})
-		}
+		ir := ImageRec{line[0], vector}
+		L.InsertImageRec(ir, matrix)
 	}
 }
 
+// a struct method to return all the keys in the map
+func (L *LSHMap) GetKeys() []string {
+	var keys []string
+	for key := range *L {
+		keys = append(keys, key)
+	}
+	return keys
+}
 
+// a struct method to return the closest bucket to a given bucketid
+func (L *LSHMap) GetClosestBucket(bucketid string) string {
+	allbuckets := L.GetKeys()
+	return FindClosestBucket(bucketid, allbuckets)
+}
+
+func FindCosineSimilarity(recs []ImageRec, vector []float64, ch chan ResType) {
+	for _, rec := range recs {
+		score := rec.CosineSimilarity(vector)
+		ch <- ResType{rec.ImageID, score}
+	}
+}
+
+func (L *LSHMap) Query(bucketid string, vector []float64)[]string{
+	var result []string
+
+	b2bsearched := L.GetClosestBucket(bucketid)
+
+	ch := make(chan ResType,4)
+	maxprocs := runtime.GOMAXPROCS(0)
+	if len((*L)[b2bsearched]) < maxprocs {
+		maxprocs = len((*L)[b2bsearched])
+	}
+	for i:=0;i<len((*L)[b2bsearched]);i+=maxprocs{
+		end := i+maxprocs
+		if end > len((*L)[b2bsearched]){
+			end = len((*L)[b2bsearched])
+		}
+		go FindCosineSimilarity((*L)[b2bsearched][i:end],vector,ch)
+	}
+
+
+
+	return result
+}
 
 // compute and return the dot product of two float64 vectors
 func dotProduct(vector1 []float64, vector2 []float64) float64 {
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-
-
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 func main() {
-
+	binary := []string{"0000", "0001", "0010", "0011", "0100", "0101","0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101","1110", "1111"}
+	maxprocs := runtime.GOMAXPROCS(0)
+	var wg sync.WaitGroup;
+	if len(binary) < maxprocs {
+		maxprocs = len(binary)
+	}
+	// iterate over the binary slice slicing it into maxprocs chunks
+	for i := 0; i < len(binary); i += maxprocs {
+		end := i + maxprocs
+		if end > len(binary) {
+			end = len(binary)
+		}
+		// launch a goroutine for each chunk
+		wg.Add(1)
+		fmt.Println("Launching goroutine")
+		go func(binary []string) {
+			defer wg.Done()
+			for _, b := range binary {
+				fmt.Println(b)
+			}
+		}(binary[i:end])
+	}
+	wg.Wait()
+	
 }
