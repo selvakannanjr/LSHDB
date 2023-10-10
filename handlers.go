a56b23f6adb1936b23bb7888d3fd3ef4bcc34669
selvakannanjr <4mselva@gmail.com>
2023-10-10 22:32:26 +0530

    removed unused handler

--- handlers.go
+++ handlers.go
@@ -1,34 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 )
-
-func HandleRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	var payload Payload
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		http.Error(w, "Error unmarshalling JSON", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintf(w, "Received: %v\n", payload.Vector)
-	fmt.Println("Received: ", payload.Vector)
-}
 
 func RateLimiter(f http.HandlerFunc, maxClients int) http.HandlerFunc {
 	sema := make(chan struct{}, maxClients)
